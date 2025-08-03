f0ce40338d9e746aa040186baeec3a3443ca644c
Seiya Fujishima <86668878+fujishimaseiya@users.noreply.github.com>
2025-08-03 16:40:27 +0900

    Remove unnecessary code from do_inv.py
    Commented-out code and debugging code have been removed from do_inv.py

--- nninv1d/do_inv.py
+++ nninv1d/do_inv.py
@@ -10,14 +10,11 @@ from nninv1d import reproduce_y, read_data, preprocess, deep_learning_turbidite,
 from validation import Validation
 
 if __name__ == "__main__":
-    # pdb.set_trace()
     data_folder = '/mnt/sed_share2/fujishima/phd_research/data/exp2/run2/no_detrainment/no_erosion/after_cfrev/cf0.002_ro2/data'
     resdir = '/mnt/sed_share2/fujishima/phd_research/data/exp2/run2/no_detrainment/no_erosion/after_cfrev/cf0.002_ro2/inv_result/test'
     if not os.path.exists(resdir):
         os.mkdir(resdir)
     cood_file = '/mnt/sed_share2/fujishima/3d_model/exp2/diff_bed_run_1_2/sed_volume_run2/sed_dx0.05.csv'
-    # cood_file = '/home/biosphere/sed_share2/fujishima/phd_research/data/exp1/detrainment/cf0.002_detrate1.0/samp_point2.csv'
-    # cood_file = '/mnt/sed_share2/fujishima/phd_research/data/exp3/exp3_cf0.002_ro2_noerosion/after_revcf/inv_result/nonormx_8000/samp_idx_exp3.csv'
     src = "nninv1d.py"
     dst = os.path.join(resdir, "nninv1d.py")
     shutil.copyfile(src, dst)
@@ -65,19 +62,6 @@ if __name__ == "__main__":
         savedir=resdir
     )
 
-    check_result_norm = model.predict(x_train)
-    check_result = reproduce_y(check_result_norm, norm_y)
-    check_original = reproduce_y(y_train, norm_y)
-    np.savetxt(os.path.join(resdir, 'check_result.csv'), check_result, delimiter=',')
-    np.savetxt(os.path.join(resdir,'check_original.csv'), check_original, delimiter=',')
-    min_val = np.min(np.min([check_original, check_result], axis=0), axis=0)
-    min_val[min_val < 0] = 0
-    max_val = np.max(np.max([check_original, check_result], axis=0), axis=0)
-    # val_name = ['$C_{1}$', '$C_{2}$','$C_{3}$','$C_{4}$', 'Flow Velocity', 'Flow Height', 'Flow Duration']
-    val_name = ['$C_{1}$', '$C_{2}$','$C_{3}$','$C_{4}$', '$S_{0}$', 'Initial Flow Velocity', 'Initial Flow Height', 'Flow Duration']
-    # val_name = ['Cf', '$r_{0}$', '$C_{1}$', '$C_{2}$','$C_{3}$','$C_{4}$', 'Salinity', 'Initial Flow Velocity', 'Flow Duration']
-    units = ['', '', '' , '', '', ' (m/s)', ' (m)', ' (s)']
-
     # validation of inverse model using flume experiment
     validation = Validation()
     validation.predict(model = model, 
@@ -92,20 +76,3 @@ if __name__ == "__main__":
                                             "bed__sediment_volume_per_unit_area_3"], 
                        val_list=['C0,1', 'C0,2', 'C0,3', 'C0,4', 'Salinity', 'U0', 'h0', 'Fd']
                        )
-    
-    for i in range(check_result.shape[1]):
-        fig, ax = plt.subplots(1, 1, figsize=(3.93, 3.93),tight_layout=True)
-        ax.plot(check_original[:, i], check_result[:, i], 'o')
-        ax.plot([min_val[i], max_val[i] * 1.1], [min_val[i], max_val[i] * 1.1],
-                marker=None,
-                linewidth=2)
-        ax.set_title(val_name[i], fontsize=18)
-        ax.set_xlabel('Original Value' + units[i],
-                        fontsize=14)
-        ax.set_ylabel('Reconstructed Value' + units[i],
-                        fontsize=14)
-        ax.tick_params(labelsize=14)
-        ax.set_aspect('equal')
-        fig.patch.set_alpha(0)
-        plt.savefig(os.path.join(resdir, 'check_result{}.svg'.format(i)))
-        ax.cla()
